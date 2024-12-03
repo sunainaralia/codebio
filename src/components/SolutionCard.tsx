51d757c0cc19b979dd9e67426586be16ca583f1d
sunainaralia <124876937+sunainaralia@users.noreply.github.com>
2024-12-03 17:47:30 +0530

    all

--- src/components/SolutionCard.tsx
+++ src/components/SolutionCard.tsx
@@ -3,7 +3,7 @@ import { Separator } from "./ui/separator";
 import LottieAnimation from "@/app/(main)/_components/lottiePlayer";
 import Image from "next/image";
 import Link from "next/link";
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
 
 interface SolutionCardProps {
