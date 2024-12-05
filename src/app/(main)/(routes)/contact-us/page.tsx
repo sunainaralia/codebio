d6d03b38cba633428ae87e683f48115d6c671bff
mj-saini <manojsaini97285@gmail.com>
2024-12-05 22:23:07 +0530

    pull mj

--- src/app/(main)/(routes)/contact-us/page.tsx
+++ src/app/(main)/(routes)/contact-us/page.tsx
@@ -10,13 +10,13 @@ function ContactUs() {
   return (
     <div className='px-[30px] md:px-0'>
       <div className='3xl:mt-[150px] lg:mt-[90px] mt-[30px] '>
-        <div className=' mx-auto text-3xl  xl:text-4xl 3xl:text-[84px] uppercase font-[900] text-center mb-[25px] '>
+        <div className=' mx-auto text-3xl  xl:text-4xl 4xl:text-[84px] uppercase font-[900] text-center mb-[25px] '>
           CONTACT US
         </div>
 
         <div className='flex w-fit gap-4  mx-auto'>
           <Image src={'/vectors/Star 1.svg'} alt='' height={36} width={36} className='h-[16px] w-[16px] md:w-[28px] md:h-[28px]  3xl:h-[36px]  3xl:w-[36px] 3xl:mt-[30px]'/>
-          <h3 className='text-md text-center md:text-xl 3xl:text-[42px] text-[#525252] font-normal 3xl:mt-[30px]'>
+          <h3 className='text-md text-center md:text-xl 4xl:text-[42px] text-[#525252] font-normal 4xl:mt-[30px]'>
             Our friendly team would love to hear from you
           </h3>
           <Image src={'/vectors/Star 1.svg'} alt='' height={36} width={36} className='3xl:h-[36px]  3xl:w-[36px] md:w-[28px] md:h-[28px] h-[16px] w-[16px] 3xl:mt-[30px]'/>
